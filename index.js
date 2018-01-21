b66909478d34d422fa5434f431909bc6f9ed3b3d
suushii <32783057+suushii@users.noreply.github.com>
2018-01-21 10:32:43 -0500

    fix style

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-
 function genEventCode() {
-  return "CD_" + Math.random().toString(36).substr(2, 4) + "-" + Math.random().toString(36).substr(3, 4);
+  return (
+    "CD_" +
+    Math.random()
+      .toString(36)
+      .substr(2, 4) +
+    "-" +
+    Math.random()
+      .toString(36)
+      .substr(3, 4)
+  );
 }
-
