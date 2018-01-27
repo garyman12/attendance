67064077ac54c70d7658db7bedcb929a7119b02d
suushii <32783057+suushii@users.noreply.github.com>
2018-01-27 13:13:07 -0500

    Add Web server

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+/* HTTPS SERVER
+var privateKey  = fs.readFileSync('/path/to/franciskim.co.key', 'utf8');
+var certificate = fs.readFileSync('/path/to/franciskim.co.crt', 'utf8');
+var credentials = { key: privateKey, cert: certificate };
+
+// START --== Your App Code ==--
+app.get('/', function(req, res, next) {
+    res.json({
+        app: 'Foobar App'
+    });
+});
+// END --== Your App Code ==--
+
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(1337);
+HTTPS SERVER */
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/attendance.html");
+});
+app.get("/login", function(req, res) {
+  res.sendFile(__dirname + "/login.html");
+});
+app.use(express.static("css"));
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
+//Generate event code.
 function genEventCode() {
   return (
     "CD_" +
